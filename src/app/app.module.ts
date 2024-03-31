38301063a0be5613871db02680b9cd177ed7183c
ponganan <joeyubu@gmail.com>
2024-03-31 22:29:29 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,22 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+//import Toggle Material 
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+//import Input Material 
+import { MatInputModule } from '@angular/material/input';
+//import select Material
+import { MatSelectModule } from '@angular/material/select';
+//import datepicker Material
+import { MatDatepickerModule } from '@angular/material/datepicker';
+//import datepicker Material have to import MatMomentDateModule
+// and import to project 
+// ng add @angular/material-moment-adapter
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+
+
 
 @NgModule({
   declarations: [
@@ -10,10 +26,21 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //import Toggle Material
+    MatSlideToggleModule,
+    //import Input Material
+    MatInputModule,
+    //import select Material
+    MatSelectModule,
+    //import datepicker Material
+    MatDatepickerModule,
+    //import datepicker Material have to import MatMomentDateModule
+    MatMomentDateModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
